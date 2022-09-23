3a75609771328cbc96d43c9ececc1ab6e53a1109
Al Zanari <alzanari@protonmail.com>
2022-09-23 12:53:53 +0000

    scafolded App component

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,10 +5,34 @@
 <script>
 
 export default {
-  name: 'App'
-}
+  name: 'App',
+  computed: {
+    currentUser() {
+      return this.$store.state.auth.user;
+    },
+    showAdminBoard() {
+      if (this.currentUser && this.currentUser['roles']) {
+        return this.currentUser['roles'].includes('ROLE_ADMIN');
+      }
+
+      return false;
+    },
+    showModeratorBoard() {
+      if (this.currentUser && this.currentUser['roles']) {
+        return this.currentUser['roles'].includes('ROLE_MODERATOR');
+      }
+
+      return false;
+    }
+  },
+  methods: {
+    logOut() {
+      this.$store.dispatch('auth/logout');
+      this.$router.push('/login');
+    }
+  }
+};
 </script>
 
 <style>
-}
 </style>
