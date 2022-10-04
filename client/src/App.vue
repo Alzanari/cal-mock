5829ca6a7d36b892092a99c25d908567aabae8a7
Al Zanari <alzanari@protonmail.com>
2022-10-04 11:41:23 +0100

    stylized the login page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,21 @@
 <template>
-  <b-container fluid id="app">
+  <b-container fluid :class="{ otherColor: isLoggingIn }" style="height: 100%">
     <router-view></router-view>
   </b-container>
 </template>
 
-<style></style>
+<script>
+export default {
+  computed: {
+    isLoggingIn() {
+      return this.$route.path === "/login";
+    },
+  },
+};
+</script>
+
+<style>
+.otherColor {
+  background-color: #63f2fcde;
+}
+</style>
